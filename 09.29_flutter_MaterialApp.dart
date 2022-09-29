aafffbee1039368ace2d896e0fa6d576703b451c
dioon0000 <91640754+dioon0000@users.noreply.github.com>
2022-09-29 18:10:08 +0900

    Scaffold() 함수를 이용하여 기본 틀 만들기
    Signed-off-by: dioon0000 <91640754+dioon0000@users.noreply.github.com>

---
+++ 09.29_flutter_MaterialApp.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title : 'Material Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MaterialFlutterApp(),
+    );
+  }
+}
+
+class MaterialFlutterApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MaterialFlutterApp();
+  }
+}
+
+class _MaterialFlutterApp extends State<MaterialFlutterApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
